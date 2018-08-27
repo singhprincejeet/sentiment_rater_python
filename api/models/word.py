6ca17efd10d2f764405c4a1a95320592b327335c
singhprincejeet <singhprincejeet@gmail.com>
2018-08-27 07:02:43 -0400

    To create model for words

---
+++ api/models/word.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from .tweet import Tweet
+
+
+class Word(models.Model):
+    word = models.CharField(max_length=140, unique=True)
+    tweets = models.ManyToManyField(Tweet)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        app_label='api'
+        ordering=('created_at',)
