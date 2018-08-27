bb54f094d66756eafa565dde2dbc4062a3063d72
singhprincejeet <singhprincejeet@gmail.com>
2018-08-27 07:02:15 -0400

    To create model for tweets

---
+++ api/models/tweet.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from .sentiment import Sentiment
+
+
+class Tweet(models.Model):
+    content = models.CharField(max_length=140)
+    sentiment = models.ForeignKey(Sentiment, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        app_label='api'
+        ordering=('created_at',)
