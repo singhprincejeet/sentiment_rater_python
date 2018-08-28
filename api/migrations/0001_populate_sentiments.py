cfe7b9457d10fcabac5e1917c5d26b1d03394110
singhprincejeet <singhprincejeet@gmail.com>
2018-08-27 22:40:43 -0400

    Populate sentiment model

---
+++ api/migrations/0001_populate_sentiments.py
@@ -0,0 +1,31 @@
+from django.db import migrations
+from api.models import Sentiment
+
+
+def populate_sentiments(_, __):
+    positive = Sentiment(
+        id=0,
+        value="positive"
+    )
+    positive.save()
+    negative = Sentiment(
+        id=4,
+        value="negative"
+    )
+    negative.save()
+
+
+
+
+def delete_sentiments(_, __):
+    Sentiment.objects.all().delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("api", "0002_sentiment")
+    ]
+    operations = [
+        migrations.RunPython(populate_sentiments, delete_sentiments),
+    ]
