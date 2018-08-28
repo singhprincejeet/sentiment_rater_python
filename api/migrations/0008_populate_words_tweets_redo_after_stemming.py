857396841cede0a0dd163552d93118aa3a2da51e
singhprincejeet <singhprincejeet@gmail.com>
2018-08-27 23:03:50 -0400

    Repopulate word model with stemmed word

---
+++ api/migrations/0008_populate_words_tweets_redo_after_stemming.py
@@ -0,0 +1,38 @@
+from nltk import word_tokenize
+from nltk.stem import SnowballStemmer
+from django.db import migrations
+
+from api.models import Tweet
+from api.models import Word
+
+
+def populate_words_tweets(_, __):
+
+    tweets = Tweet.objects.all()
+
+    i = 0
+
+    snowball_stemmer = SnowballStemmer("english")
+
+    for tweet in tweets.all():
+        tokens_from_tweet = word_tokenize(str(tweet.content).lower())
+        for token in tokens_from_tweet:
+            try:
+                token = snowball_stemmer.stem(token)
+                word = Word.objects.get(word=token)
+            except Word.DoesNotExist:
+                continue
+            word.tweets.add(tweet)
+        if i % 1000 == 0:
+            print(str(i) + " tweets done")
+        i += 1
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("api", "0008_stem_words")
+    ]
+    operations = [
+        migrations.RunPython(populate_words_tweets),
+    ]
