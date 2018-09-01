f47482ed7f0573e5e0120852331463061c2cb1d7
singhprincejeet <singhprincejeet@gmail.com>
2018-09-01 18:59:15 -0400

    changed field id to charfield to save twitter handles

--- api/models/user.py
+++ api/models/user.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class User(models.Model):
+    id = models.CharField(max_length=40, unique=True, primary_key=True)
     reputation = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
