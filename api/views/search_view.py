33adc391ccc4311e905297b6dd7fd670588bb70e
singhprincejeet <singhprincejeet@gmail.com>
2018-09-01 13:32:11 -0400

    search for multiple keywords, the result with most keywords go on top

--- api/views/search_view.py
+++ api/views/search_view.py
@@ -12,19 +12,9 @@ from api.serializers import TweetSerializer
 def search(request, query):
 
     try:
-        snowball_stemmer = SnowballStemmer("english")
-        tokens = word_tokenize(query)
-        tokens = [token for token in tokens if token not in stopwords.words('english')]
-        words = []
-        tweets = []
-        for token in tokens:
-            token = snowball_stemmer.stem(token)
-            words.append(Word.objects.get(word=token))
-        for word in words:
-            for tweet in word.tweets.all():
-                tweets.append(tweet)
-        if len(tweets) > 10:
-            tweets = tweets[:10]
+        tokens = tokenize(query.lower())
+        words = get_words(tokens)
+        tweets = get_tweets(words)
     except Word.DoesNotExist:
         return HttpResponse(status=404)
     except Tweet.DoesNotExist:
@@ -33,3 +23,33 @@ def search(request, query):
     if request.method == 'GET':
         serializer = TweetSerializer(tweets, many=True)
         return JsonResponse(serializer.data, safe=False)
+
+
+def tokenize(query):
+    tokens = word_tokenize(query)
+    return [token for token in tokens if token not in stopwords.words('english')]
+
+
+def get_words(tokens):
+    snowball_stemmer = SnowballStemmer("english")
+    words = []
+    for token in tokens:
+        token = snowball_stemmer.stem(token)
+        words.append(Word.objects.get(word=token))
+    return words
+
+
+def get_tweets(words):
+    words.sort(key=lambda word: word.frequency, reverse=True)
+    tweets_objects = [word.tweets for word in words]
+    tweets_dict = {}
+    for tweets_object in tweets_objects:
+        tweets = tweets_object.all()
+        for tweet in tweets:
+            if tweet in tweets_dict:
+                tweets_dict[tweet] += 1
+            else:
+                tweets_dict[tweet] = 1
+    tweets_dict = sorted(tweets_dict.items(), key=lambda x:x[1], reverse=True)
+    result = [tweet[0] for tweet in tweets_dict]
+    return result
