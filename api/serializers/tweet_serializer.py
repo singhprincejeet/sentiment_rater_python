4070933a0d6c74dcc5ac2c965b6f0d4ecff8d92d
singhprincejeet <singhprincejeet@gmail.com>
2018-09-04 01:41:57 -0400

    add user field to tweet serializer

--- api/serializers/tweet_serializer.py
+++ api/serializers/tweet_serializer.py
@@ -5,7 +5,8 @@ from api.models import Tweet
 
 class TweetSerializer(serializers.ModelSerializer):
     sentiment = serializers.SlugRelatedField(slug_field='value', read_only=True)
+    user = serializers.SlugRelatedField(slug_field='id', read_only=True)
 
     class Meta:
         model = Tweet
-        fields = ('content', 'sentiment')
+        fields = ('content', 'sentiment', 'user')
